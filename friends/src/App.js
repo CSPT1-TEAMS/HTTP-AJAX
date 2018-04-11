b2621f904a85a8067bc1c2bb5e0db17893f06a82
JPEmery <clearkeyemery@gmail.com>
2018-04-10 21:54:08 -0500

    JP Emery: connection to server compelete

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 class App extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    friends: [],   
+  }
+}
+
+componentDidMount() {
+  axios.get('http://localhost:5000/friends')
+  .then(response => {this.setState( {friends: response.data})})
+  .catch(err => {console.log(err)});
+}
+
+
+
+
   render() {
+
+    console.log(this.state);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="App">
+      {this.state.friends.map(friend => {
+        return (
+          <div>
+          {friend.name}
+          </div>)
+      })}
+
+
+     
+      
       </div>
     );
   }
