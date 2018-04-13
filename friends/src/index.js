8cd4695a46e839a53cd667182cef38195867b0f0
wlharvey4 <pinecone062@gmail.com>
2018-04-13 09:13:11 -0700

    Started on stretch goals; added BrowserRouter

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,22 @@
+/**
+   index.js
+   ========
+   Created 2018-04-13T09:11:55
+   Version 0.1.1
+   Added Browser Router
+   ------------------------------
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
