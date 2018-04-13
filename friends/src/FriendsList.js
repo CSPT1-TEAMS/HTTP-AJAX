9af4d7535b22e70e7b7f59fce8621e0bff69aa04
wlharvey4 <pinecone062@gmail.com>
2018-04-13 08:36:35 -0700

    Dan's stretch goal solution code

--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import './Friends.css';
+import './Friends.css'
+import AddFriend from './AddFriend'
 
 export default class FriendsList extends Component {
   constructor(){
     super()
     this.state = {
       friends: [],
-      name: '',
-      age: '',
-      email: ''
     }
   }
 
@@ -25,15 +23,11 @@ export default class FriendsList extends Component {
          .catch(err => console.log(err))
   }
 
-  nameChange = (e) => { this.setState({name: e.target.value})}
-  ageChange = (e) => { this.setState({ age: e.target.value }) }
-  emailChange = (e) => { this.setState({ email: e.target.value }) }
-  buttonSubmit = () => {
-    const { name, age, email } = this.state
-    axios.post('http://localhost:5000/friends', { name, age, email})
-                                     .then( (response) => {
-                                       this.setState({ friends: response.data, name: '', age: '', email: ''})
-                                     })
+  buttonSubmit = (friend) => {
+    axios.post('http://localhost:5000/friends', friend)
+         .then( (response) => {
+           this.setState({ friends: response.data, name: '', age: '', email: ''})
+         })
   }
   render() {
     return (
@@ -50,11 +44,7 @@ export default class FriendsList extends Component {
              )
           })}
         </ul>
-        <h3>Add Friend</h3>
-        <input type='text' placeholder='Name' onChange={this.nameChange} value={this.state.name}/>
-        <input type='number' placeholder='Age' onChange={this.ageChange} value={this.state.age}/>
-        <input type='text' placeholder='Email' onChange={this.emailChange} value={this.state.email}/>
-        <button onClick={this.buttonSubmit}>Submit</button>
+        <AddFriend buttonSubmit={this.buttonSubmit} />
       </div>
     )
   }
