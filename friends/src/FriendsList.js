5a1c6bef8fc5d57063f60d8c98028ae71249b73c
Greg Knudsen <dev.artist15@gmail.com>
2018-04-13 09:02:23 -0400

    main assignment

--- friends/src/FriendsList.js
+++ friends/src/FriendsList.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './FriendsList.css';
 
 export default class FriendsList extends Component {
   constructor(props){
     super(props)
     this.state = {
-      friends: []
+      friends: [],
+      name: '',
+      age: '',
+      email: ''
     }
   }
 
@@ -17,15 +21,29 @@ export default class FriendsList extends Component {
       .catch((error) => console.log(error));
   }
 
-  render() {
+  nameChange = (e) => { this.setState({name: e.target.value }) }
+  ageChange = (e) => { this.setState({ age: e.target.value }) }
+  emailChange = (e) => { this.setState({ email: e.target.value }) }
+  buttonSubmit = () => {
+    const { name, age, email } = this.state
+    axios.post('http://localhost:5000/friends', {
+      name,
+      age,
+      email
+    })
+      .then((response) => {
+        this.setState({friends: response.data, name: '', age: '', email: ''})
+      })
+  }
+   render() {
 
     return(
       <div>
         <h1>Friends:</h1>
-        <ul>
-          {this.state.friends.map((friend, i) => {
+        <ul className="friends">
+          {this.state.friends.map((friend) => {
             return (
-              <li key={friend.id}>
+              <li className="friend" key={friend.id}>
                 <p>Name: {friend.name}</p>
                 <p>Age: {friend.age}</p>
                 <p>Email: {friend.email}</p>
@@ -33,6 +51,11 @@ export default class FriendsList extends Component {
             )
           })}
         </ul>
+        <h3>Add Friend</h3>
+        <input type='text' placeholder='Name' onChange={this.nameChange} value={this.state.name}/>
+        <input type='number' placeholder='Age' onChange={this.ageChange} value={this.state.age}/>
+        <input type='text' placeholder='Email' onChange={this.emailChange} value={this.state.email}/>
+        <button onClick={this.buttonSubmit}>Submit</button>
       </div>
     );
   }
